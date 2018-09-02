bb4039ac527ddcc761f44f7b72eefe13fc62cd54
shayshae5482 <39655763+shayshae5482@users.noreply.github.com>
2018-09-02 18:22:26 -0400

    Got the prompts to work.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -15,31 +16,87 @@ var connection = mysql.createConnection({
 });
 
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    queryAllIds();
-    queryAllProducts();
-  });
-  
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  queryAllIds();
+  //queryAllProducts();
+});
+
+//Prompt the user on how they want to search for products
+
+function start(data) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "id",
+        message: "Enter the product id",
+      },
+
+      {
+        type: "input",
+        name: "units",
+        message: "How many would you like?"
+      }
+    
+      
+
+    ])
+.then(function(answer) {
+
+  var quantity;
+  for (var i =0; i < data.length; i++) {
+if (data[i].item_id==answer.id) {
+  quantity = data[i].stock_quantity;
+}
+
+  }
+
+if (answer.units > quantity) {
+  console.log("Insufficient quantity. Try again.");
+
+  return start(data);
+}
+connection.query(
+  "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+  [
+{
+  stock_quantity: answer.units
+},
+
+{
+  item_id: answer.id
+}
+
+  ]
+)
+}
+
+)
+}  
+
+
 
 
 function queryAllIds() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      for (var i = 0; i < res.length; i++) {
-        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        connection.query("SELECT * FROM products", function (err, res) {
+          for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+          }
+          console.log("-----------------------------------");
+          start (res)
+        });
       }
-      console.log("-----------------------------------");
-    });
-  }
   
   function queryAllProducts() {
-     var query = connection.query("SELECT * FROM products WHAT kind=?", ["product_name"], function(err, res) {
-       for (var i = 0; i < res.length; i++) {
-        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        var query = connection.query("SELECT * FROM products WHERE kind=?", ["product_name"], function (err, res) {
+          console.log(res);
+          //    for (var i = 0; i < res.length; i++) {
+          //     console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+          //   }
+        });
+
+        //logs the actual query being run
+        console.log(query.sql);
       }
-     });
-  
-      //logs the actual query being run
-     console.log(query.sql);
-   }
